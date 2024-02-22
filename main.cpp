67d6add614e8c1a80d59ae0665b883d583626257
Bilou4000 <claire.benes@artfx.fr>
2024-02-22 16:14:35 +0100

    [feat] case switch for main Menu, Play and End

--- main.cpp
+++ main.cpp
@@ -4,9 +4,21 @@
 
 using namespace std;
 
+typedef enum GameScreen { MENU, GAMEPLAY, ENDING } GameScreen;
+GameScreen currentScreen;
+
 //Screen
 int screenWidth = 1600;
 int screenHeight = 900;
+
+////MiddleLine
+//struct MiddleLine
+//{
+//    Rectangle rect;
+//};
+//
+//Rectangle rect { 830 - rect.width / 2, 0, 30, 40 };
+//MiddleLine middleLine { rect };
 
 //Ball
 Rectangle ball { 100, 150, 64, 64 };
@@ -30,15 +42,13 @@ int rightScore = 0;
 string textResult;
 bool resultVisible = false;
 
-//GameState
-int gameState = 0;
-
 //Function
 void Load();
 void Update();
 void Draw();
 void Unload();
 
+void Gameplay();
 bool Collision(Rectangle a, Rectangle b);
 void BounceOnPaddle(bool playerSide);
 void PlaceBall(bool playerSide);
@@ -62,104 +72,138 @@ int main()
 void Load() 
 {
     InitWindow(screenWidth, screenHeight, "RAYLIB PROJECT");
+    currentScreen = MENU;
     SetTargetFPS(60);
 }
 
 void Update() 
 {
-    if (gameState == 0)
+    switch (currentScreen)
     {
-        //BALL
-        ball.x += speedXBall;
-        ball.y += speedYBall;
-
-        if (ball.y < 0)
+        case MENU:
         {
-            speedYBall = -speedYBall;
-            ball.y = 0;
-        }
-        if (ball.y + ball.height > screenHeight)
-        {
-            speedYBall = -speedYBall;
-            ball.y = screenHeight - ball.height;
-        }
-
-        if (ball.x < 0)
-        {
-            ++rightScore;
-            PlaceBall(true);
-
-            if (rightScore >= 3)
+            if (IsKeyPressed(KEY_ENTER))
             {
-                gameState = 2;
-                textResult = "Defeat";
-                resultVisible = true;
+                currentScreen = GAMEPLAY;
             }
         }
-        if (ball.x + ball.width > screenWidth)
+        break;
+        case GAMEPLAY:
         {
-            ++leftScore;
-            PlaceBall(false);
-
-            if (leftScore >= 3)
+            Gameplay();
+        }
+        break;
+        case ENDING:
+        {
+            if (IsKeyPressed(KEY_R))
             {
-                gameState = 1;
-                textResult = "Victory";
-                resultVisible = true;
+                textResult = "";
+                leftScore = 0;
+                rightScore = 0;
+
+                currentScreen = GAMEPLAY;
+            }
+
+            if (IsKeyPressed(KEY_SEMICOLON))
+            {
+                textResult = "";
+                leftScore = 0;
+                rightScore = 0;
+
+                currentScreen = MENU;
             }
         }
+        break;
+        default: break;
+    }
+}
 
-        //LEFT PADDLE
-        if (IsKeyDown(KEY_S))
-        {
-            leftPaddle.y += speedLeftPaddle;
-        }
-        else if (IsKeyDown(KEY_W))
-        {
-            leftPaddle.y -= speedLeftPaddle;
-        }
+void Gameplay()
+{
+    //BALL
+    ball.x += speedXBall;
+    ball.y += speedYBall;
 
-        if (leftPaddle.y < 0)
-        {
-            leftPaddle.y = 0;
-        }
-        if (leftPaddle.y + leftPaddle.height > screenHeight)
-        {
-            leftPaddle.y = screenHeight - leftPaddle.height;
-        }
+    if (ball.y < 0)
+    {
+        speedYBall = -speedYBall;
+        ball.y = 0;
+    }
+    if (ball.y + ball.height > screenHeight)
+    {
+        speedYBall = -speedYBall;
+        ball.y = screenHeight - ball.height;
+    }
 
-        if (rightPaddle.y < 0)
-        {
-            rightPaddle.y = 0;
-        }
-        if (rightPaddle.y + rightPaddle.height > screenHeight)
-        {
-            rightPaddle.y = screenHeight - rightPaddle.height;
-        }
+    if (ball.x < 0)
+    {
+        ++rightScore;
+        PlaceBall(true);
 
-        //COLLISION
-        if (Collision(leftPaddle, ball))
+        if (rightScore >= 3)
         {
-            BounceOnPaddle(true);
-        }
-        else if (Collision(rightPaddle, ball))
-        {
-            BounceOnPaddle(false);
-        }
-
-        //IA Right Paddle
-        if (ball.y < rightPaddle.y + rightPaddle.height / 4)
-        {
-            rightPaddle.y -= speedRightPaddle;
-        }
-        else if (ball.y > rightPaddle.y + rightPaddle.height * 3 / 4)
-        {
-            rightPaddle.y += speedRightPaddle;
+            textResult = "Defeat";
+            currentScreen = ENDING;
         }
     }
-    else
+    if (ball.x + ball.width > screenWidth)
     {
-        //
+        ++leftScore;
+        PlaceBall(false);
+
+        if (leftScore >= 3)
+        {
+            textResult = "Victory";
+            currentScreen = ENDING;
+        }
+    }
+
+    //LEFT PADDLE
+    if (IsKeyDown(KEY_S))
+    {
+        leftPaddle.y += speedLeftPaddle;
+    }
+    else if (IsKeyDown(KEY_W))
+    {
+        leftPaddle.y -= speedLeftPaddle;
+    }
+
+    if (leftPaddle.y < 0)
+    {
+        leftPaddle.y = 0;
+    }
+    if (leftPaddle.y + leftPaddle.height > screenHeight)
+    {
+        leftPaddle.y = screenHeight - leftPaddle.height;
+    }
+
+    if (rightPaddle.y < 0)
+    {
+        rightPaddle.y = 0;
+    }
+    if (rightPaddle.y + rightPaddle.height > screenHeight)
+    {
+        rightPaddle.y = screenHeight - rightPaddle.height;
+    }
+
+    //COLLISION
+    if (Collision(leftPaddle, ball))
+    {
+        BounceOnPaddle(true);
+    }
+    else if (Collision(rightPaddle, ball))
+    {
+        BounceOnPaddle(false);
+    }
+
+    //IA Right Paddle
+    if (ball.y < rightPaddle.y + rightPaddle.height / 4)
+    {
+        rightPaddle.y -= speedRightPaddle;
+    }
+    else if (ball.y > rightPaddle.y + rightPaddle.height * 3 / 4)
+    {
+        rightPaddle.y += speedRightPaddle;
     }
 }
 
@@ -196,11 +240,11 @@ void PlaceBall(bool playerSide)
 {
     if (playerSide)
     {
-        ball.x = 250;
+        ball.x = ball.width * 4;
     }
     else
     {
-        ball.x = screenWidth - 250;
+        ball.x = screenWidth - ball.width * 4;
     }
 
     ball.y = 150;
@@ -212,16 +256,42 @@ void Draw()
     BeginDrawing();
     ClearBackground(BLACK);
 
-    DrawText(to_string(leftScore).c_str(), 600, 50, 60, LIGHTGRAY);
-    DrawText(to_string(rightScore).c_str(), 1000, 50, 60, LIGHTGRAY);
-
-    DrawRectangleRec(ball, WHITE);
-    DrawRectangleRec(leftPaddle, WHITE);
-    DrawRectangleRec(rightPaddle, WHITE);
-
-    if (resultVisible)
+    switch (currentScreen)
     {
-        DrawText(textResult.c_str(), 700, 200, 80, LIGHTGRAY);
+        case MENU:
+        {
+            DrawText("PONG", 580, 150, 150, RED);
+            DrawText("Press ENTER to PLAY", 350, 400, 80, LIGHTGRAY);
+        }
+        break;
+        case GAMEPLAY:
+        {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    DrawRectangleRec(middleLine.rect, WHITE);
+            //    rect.y += 10;
+            //}
+
+
+            DrawRectangleRec(ball, WHITE);
+            DrawRectangleRec(leftPaddle, BLUE);
+            DrawRectangleRec(rightPaddle, RED);
+
+            DrawText(to_string(leftScore).c_str(), 600, 50, 60, BLUE);
+            DrawText(to_string(rightScore).c_str(), 1000, 50, 60, RED);
+        }
+        break;
+        case ENDING:
+        {
+            //DrawText(to_string(leftScore).c_str(), 600, 50, 60, LIGHTGRAY);
+            //DrawText(to_string(rightScore).c_str(), 1000, 50, 60, LIGHTGRAY);
+
+            DrawText(textResult.c_str(), 680, 200, 80, LIGHTGRAY);
+            DrawText("Press R to PLAY AGAIN", 350, 400, 80, LIGHTGRAY);
+            DrawText("Or Press M to go back to MENU", 150, 500, 80, LIGHTGRAY);
+        }
+        break;
+        default: break;
     }
 
     EndDrawing();
